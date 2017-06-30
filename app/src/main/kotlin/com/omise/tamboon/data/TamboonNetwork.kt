2da1e612417a8c61cf5d387e59c72c97c1dfaa47
Damrongsiri, Prisa (Agoda) <prisa.damrongsiri@agoda.com>
2017-06-30 23:57:01 +0700

    remove client

--- app/src/main/kotlin/com/omise/tamboon/data/TamboonNetwork.kt
+++ app/src/main/kotlin/com/omise/tamboon/data/TamboonNetwork.kt
@@ -1,7 +1,5 @@
 package com.omise.tamboon.data
 
-import com.omise.tamboon.LoggingInterceptor
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,7 +11,6 @@ class TamboonNetwork {
                 .baseUrl("http://www.mocky.io/v2/")
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
-                .client(OkHttpClient().apply { interceptors().add(LoggingInterceptor()) })
                 .build()
                 .create(TamboonApi::class.java)
     }
